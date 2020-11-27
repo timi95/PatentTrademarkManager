598351fc0442b072014207a97db4be2fa71032f1
Timi <timi.ogunkeye@gmail.com>
2020-11-27 19:05:32 +0000

    first commit

---
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/PatentTrademarkManagerApplication.kt
@@ -0,0 +1,11 @@
+package com.AnO.PatentTrademarkManager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PatentTrademarkManagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<PatentTrademarkManagerApplication>(*args)
+}
---
+++ src/test/kotlin/com/AnO/PatentTrademarkManager/PatentTrademarkManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.AnO.PatentTrademarkManager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PatentTrademarkManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
