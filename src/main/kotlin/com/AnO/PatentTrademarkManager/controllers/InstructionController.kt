ee988dd3cbeb0e92be72bbc521d2887b14ac0dc9
Timi <timi.ogunkeye@gmail.com>
2020-11-28 14:11:28 +0000

    InstructionController.kt rename

--- src/main/kotlin/com/AnO/PatentTrademarkManager/controllers/InstructionController.kt
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/controllers/InstructionController.kt
@@ -1,13 +1,12 @@
 package com.AnO.PatentTrademarkManager.controllers
 
-import org.springframework.stereotype.Controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class StartController {
+class InstructionController {
 
   @GetMapping("/start")
   fun start(): String {
