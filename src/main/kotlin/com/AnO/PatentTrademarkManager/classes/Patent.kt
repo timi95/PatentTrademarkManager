6de0a7438b19210eb1566ebd142b95b2e6bbe831
Timi <timi.ogunkeye@gmail.com>
2020-11-29 16:48:24 +0000

    id type changed to Long

--- src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Patent.kt
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Patent.kt
@@ -7,55 +7,37 @@ import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
 
-
-/*
-* our_reference_number:
-client_id:
-clients_reference_number:
-curent_instruction:
-date_of_instruction:
-name_of_client:
-clients_address:
-clients_contact_person:
-name_of_patentee:
-patentees_address:
-date_instruction_received:
-lawyer_responsible:
-invention_description:
-patent_registration_number:
-convention_country:
-current_status:*/
 @Entity
 data class Patent(
-        @Id @GeneratedValue val id:Int,
+        @Id @GeneratedValue val id:Long?=null,
 
-        val our_reference_number: String,
-        val client_id:String,
-        val clients_reference_number:String,
-        val curent_instruction:String,
-        val date_of_instruction: LocalDateTime,
-        val name_of_client:String,
-        val clients_address: String,
-        val clients_contact_person:String,
-        val name_of_patentee:String,
-        val patentees_address:String,
-        val date_instruction_received: LocalDateTime,
-        val lawyer_responsible:String,
-        val invention_description:String,
-        val patent_registration_number:String,
-        val convention_country:String,
-        val current_status:String,
-        val certificate_procurement_due_date:LocalDateTime,
-        val month_certificate_procurement_due:String,
-        val date_outgoing_abuja_schedule:LocalDateTime,
-        val date_incoming_abuja_schedule:LocalDateTime,
-        val date_completed_job_received: LocalDateTime,
-        val official_fee: String,
-        val facilitation: String,
-        val incentive_due_clerk: String,
-        val clerk_responsible: String,
-        val month_incoming_abuja_schedule: String,
-        val month_outgoing_abuja_schedule: String,
-        val filing_receipt_status: String,
-        val applicable_service_charge: String,
-        val quickteller_fee: String ):Instruction
+        val our_reference_number: String?=null,
+        val client_id:String?=null,
+        val clients_reference_number:String?=null,
+        val curent_instruction:String?=null,
+        val date_of_instruction: LocalDateTime?=null,
+        val name_of_client:String?=null,
+        val clients_address: String?=null,
+        val clients_contact_person:String?=null,
+        val name_of_patentee:String?=null,
+        val patentees_address:String?=null,
+        val date_instruction_received: LocalDateTime?=null,
+        val lawyer_responsible:String?=null,
+        val invention_description:String?=null,
+        val patent_registration_number:String?=null,
+        val convention_country:String?=null,
+        val current_status:String?=null,
+        val certificate_procurement_due_date:LocalDateTime?=null,
+        val month_certificate_procurement_due:String?=null,
+        val date_outgoing_abuja_schedule:LocalDateTime?=null,
+        val date_incoming_abuja_schedule:LocalDateTime?=null,
+        val date_completed_job_received: LocalDateTime?=null,
+        val official_fee: String?=null,
+        val facilitation: String?=null,
+        val incentive_due_clerk: String?=null,
+        val clerk_responsible: String?=null,
+        val month_incoming_abuja_schedule: String?=null,
+        val month_outgoing_abuja_schedule: String?=null,
+        val filing_receipt_status: String?=null,
+        val applicable_service_charge: String?=null,
+        val quickteller_fee: String?=null ):Instruction
