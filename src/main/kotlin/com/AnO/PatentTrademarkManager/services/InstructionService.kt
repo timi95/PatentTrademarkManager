d09a064da21ea0072f3bc675ddb6451b6423fdb4
Timi <timi.ogunkeye@gmail.com>
2020-11-29 16:50:30 +0000

    basic crud services for Patent implementations(WIP)

--- src/main/kotlin/com/AnO/PatentTrademarkManager/services/InstructionService.kt
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/services/InstructionService.kt
@@ -7,29 +7,42 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
-class InstructionService @Autowired constructor(
-        val patentRepository: PatentRepository,
-        val trademarkRepository:TrademarkRepository) {
+class InstructionService {
+    @Autowired
+    lateinit var patentRepository: PatentRepository
+    @Autowired
+    lateinit var trademarkRepository:TrademarkRepository
+
+    fun getPatents():List<Patent>?{
+        return this.patentRepository.findAll()
+    }
+    fun getPatent(id:Long): Patent {
+        return this.patentRepository.findById(id).get()
+    }
+
     fun createPatent(patent:Patent): Patent? {
-        try {
-            this.patentRepository.save(patent)
-        } catch (e: Exception){throw (e)}
+        try { this.patentRepository.save(patent) }
+        catch (e: Exception){throw (e)}
         return null
     }
-    fun updatePatent(
-        id:Long,
-        patent:Patent =
-        this.patentRepository.
-        findById(id).get()): Patent?{
-        try {
-            this.patentRepository.save(patent)
-        } catch (e: Exception){throw (e)}
-        return null
+
+    fun updatePatent(id:Long, patent:Patent): Patent? {
+        val check = this.patentRepository.findById(id)
+        if (!check.isPresent)
+            return throw(Exception("Patent of id:${id} does not exist"))
+            try {
+                val confirm  = patent.copy(id=id)
+                this.patentRepository.save(confirm)
+                return confirm
+            } catch (e: Exception){throw (e)}
     }
 
     fun deletePatent(id: Long):Unit?{
         try {
-            return this.patentRepository.delete(this.patentRepository.findById(id).get())
-        }catch (e: Exception){throw (e)}
+            return this.patentRepository
+                    .delete(this.patentRepository.findById(id).get()) }
+        catch (e: Exception){throw (e)}
     }
+
+
 }
