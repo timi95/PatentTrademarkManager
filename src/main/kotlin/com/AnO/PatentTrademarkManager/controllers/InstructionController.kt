ed7f7a07499e30b1c9e609a333e71b1a4daa1577
Timi <timi.ogunkeye@gmail.com>
2020-11-29 16:51:11 +0000

    crud controllers for Patent added (WIP)

--- src/main/kotlin/com/AnO/PatentTrademarkManager/controllers/InstructionController.kt
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/controllers/InstructionController.kt
@@ -1,17 +1,19 @@
 package com.AnO.PatentTrademarkManager.controllers
 
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import com.AnO.PatentTrademarkManager.classes.Patent
+import com.AnO.PatentTrademarkManager.services.InstructionService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.*
 //import io.swagger.annotations.Api
-import org.springframework.web.bind.annotation.RequestMapping
 
 
 @RestController
 @RequestMapping("/Instruction")
 //@Api(value = "Instruction", description = "Rest API for Instructions", tags = arrayOf("User API"))
 class InstructionController {
-
+  @Autowired
+  lateinit var instructionService:InstructionService
 
   @GetMapping("/start")
   fun start(): String {
@@ -19,5 +21,24 @@ class InstructionController {
     return "Hello World"
   }
 
+  @GetMapping("/patent")
+  fun getAllPatents() =
+          this.instructionService.getPatents()
+
+  @GetMapping("/patent/{id}")
+  fun getPatent(@RequestParam id: Long) =
+          this.instructionService.getPatent(id)
+
+  @PostMapping("/patent")
+  fun createPatent(@RequestBody patent: Patent) =
+          this.instructionService.createPatent(patent)
+
+  @PutMapping("/patent/{id}")
+  fun updatePatent(@RequestParam id:Long, @RequestBody patent: Patent) =
+          this.instructionService.updatePatent(id,patent)
+
+  @DeleteMapping("/patent/{id}")
+  fun deletePatent(@RequestParam id: Long) =
+          this.instructionService.deletePatent(id)
 
 }
