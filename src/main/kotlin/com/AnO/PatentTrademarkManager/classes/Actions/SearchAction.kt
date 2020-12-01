51f04ca0f9fe03d4309ad797838c86f17869f9e8
Timi <timi.ogunkeye@gmail.com>
2020-12-01 00:17:53 +0000

    Json type name specification on concrete class

--- src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/SearchAction.kt
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/SearchAction.kt
@@ -2,7 +2,9 @@ package com.AnO.PatentTrademarkManager.classes.Actions
 
 import com.AnO.PatentTrademarkManager.classes.Patent
 import com.AnO.PatentTrademarkManager.intefaces.Action
+import com.fasterxml.jackson.annotation.JsonTypeName
 import javax.persistence.*
 
 @Entity
-class SearchAction: Action()
+@JsonTypeName("search_action")
+data class SearchAction(override val instruction_id:Long?=null): Action()
