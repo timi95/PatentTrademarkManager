3197dc26ffb883d9e17098bee2945a6605cdd5a5
Timi <timi.ogunkeye@gmail.com>
2020-12-01 00:18:54 +0000

    Json type info specification on abstract class with potential for extension with subtypes

--- src/main/kotlin/com/AnO/PatentTrademarkManager/interfaces/Action.kt
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/interfaces/Action.kt
@@ -1,6 +1,21 @@
 package com.AnO.PatentTrademarkManager.intefaces
 
+import com.AnO.PatentTrademarkManager.classes.Actions.SearchAction
+import com.fasterxml.jackson.annotation.JsonSubTypes
+import com.fasterxml.jackson.annotation.JsonTypeInfo
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+
 import javax.persistence.*
 
+
+/*
+this works for this type info structure
+ {
+"action": "search_action"
+}*/
 @Entity
-abstract class Action(@Id @GeneratedValue open val id:Long?=null)
+@JsonTypeInfo(use=JsonTypeInfo.Id.NAME, include=JsonTypeInfo.As.PROPERTY, property="type_id")
+@JsonSubTypes(
+        JsonSubTypes.Type(value = SearchAction::class, name ="search_action"),)
+abstract class Action(@Id @GeneratedValue open val id:Long?=null,
+                      open val instruction_id: Long?=null)
--- src/main/kotlin/com/AnO/PatentTrademarkManager/interfaces/Instruction.kt
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/interfaces/Instruction.kt
@@ -1,6 +1,12 @@
 package com.AnO.PatentTrademarkManager.intefaces
 
+import com.AnO.PatentTrademarkManager.classes.Patent
+import com.fasterxml.jackson.annotation.JsonSubTypes
+import com.fasterxml.jackson.annotation.JsonTypeInfo
 import javax.persistence.*
 
 @Entity
+@JsonTypeInfo(use= JsonTypeInfo.Id.NAME, include= JsonTypeInfo.As.PROPERTY, property="type_id")
+@JsonSubTypes(
+        JsonSubTypes.Type(value = Patent::class, name ="patent"),)
 abstract class Instruction(@Id @GeneratedValue open val id:Long?=null)
