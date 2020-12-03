42db468ce0cb3a5e867f7856027b81406936e358
Timi <timi.ogunkeye@gmail.com>
2020-12-03 13:48:53 +0000

    redundant class bodies

--- src/main/kotlin/com/AnO/PatentTrademarkManager/repositories/PatentRepository.kt
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/repositories/PatentRepository.kt
@@ -2,9 +2,8 @@ package com.AnO.PatentTrademarkManager.repositories
 
 import com.AnO.PatentTrademarkManager.classes.Instructions.Patent
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
-interface PatentRepository : JpaRepository<Patent, Long> //{
-//    fun findBySlug(slug: String): Patent?
-//    fun findAllByOrderByAddedAtDesc(): Iterable<Patent>
-//}
+@Repository
+interface PatentRepository : JpaRepository<Patent, Long>
 
--- src/main/kotlin/com/AnO/PatentTrademarkManager/repositories/TrademarkRepository.kt
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/repositories/TrademarkRepository.kt
@@ -2,6 +2,7 @@ package com.AnO.PatentTrademarkManager.repositories
 
 import com.AnO.PatentTrademarkManager.classes.Instructions.Trademark
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
-interface TrademarkRepository: JpaRepository<Trademark,Long> {
-}
+@Repository
+interface TrademarkRepository: JpaRepository<Trademark,Long>
