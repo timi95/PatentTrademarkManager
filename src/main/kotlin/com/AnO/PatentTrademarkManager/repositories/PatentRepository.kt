9d7321ff8570907b26dbd16a4fbe5c75391b28ec
Timi <timi.ogunkeye@gmail.com>
2020-12-03 17:36:22 +0000

    uuid switch from Long

--- src/main/kotlin/com/AnO/PatentTrademarkManager/repositories/PatentRepository.kt
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/repositories/PatentRepository.kt
@@ -3,7 +3,8 @@ package com.AnO.PatentTrademarkManager.repositories
 import com.AnO.PatentTrademarkManager.classes.Instructions.Patent
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
-interface PatentRepository : JpaRepository<Patent, Long>
+interface PatentRepository : JpaRepository<Patent, UUID>
 
--- src/main/kotlin/com/AnO/PatentTrademarkManager/repositories/TrademarkRepository.kt
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/repositories/TrademarkRepository.kt
@@ -3,6 +3,7 @@ package com.AnO.PatentTrademarkManager.repositories
 import com.AnO.PatentTrademarkManager.classes.Instructions.Trademark
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
-interface TrademarkRepository: JpaRepository<Trademark,Long>
+interface TrademarkRepository: JpaRepository<Trademark, UUID>
