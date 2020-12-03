3b5935a47cc2a49dc91393e541e54bb881cb10f9
Timi <timi.ogunkeye@gmail.com>
2020-12-03 17:35:59 +0000

    uuid switch from Long and action_list added to Instruction.kt

--- src/main/kotlin/com/AnO/PatentTrademarkManager/interfaces/Instruction.kt
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/interfaces/Instruction.kt
@@ -3,10 +3,15 @@ package com.AnO.PatentTrademarkManager.intefaces
 import com.AnO.PatentTrademarkManager.classes.Instructions.Patent
 import com.fasterxml.jackson.annotation.JsonSubTypes
 import com.fasterxml.jackson.annotation.JsonTypeInfo
+import java.util.*
 import javax.persistence.*
 
 @Entity
 @JsonTypeInfo(use= JsonTypeInfo.Id.NAME, include= JsonTypeInfo.As.PROPERTY, property="type_id")
 @JsonSubTypes(
         JsonSubTypes.Type(value = Patent::class, name ="patent"),)
-abstract class Instruction(@Id @GeneratedValue open val id:Long?=null)
+abstract class Instruction(
+        @Id @GeneratedValue open val id: UUID?=null,
+        open val type_id:String?=null,
+        @OneToMany(fetch = FetchType.LAZY, cascade = arrayOf(CascadeType.ALL))
+        open val action_list: MutableList<Action>?=null)
