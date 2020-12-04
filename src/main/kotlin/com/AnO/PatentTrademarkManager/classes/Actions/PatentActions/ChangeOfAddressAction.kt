2d15938c5744490ed8f03a25e443b85b1927b236
Timi <timi.ogunkeye@gmail.com>
2020-12-04 18:25:59 +0000

    ChangeOfAddressAction.kt added

---
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/PatentActions/ChangeOfAddressAction.kt
@@ -0,0 +1,31 @@
+package com.AnO.PatentTrademarkManager.classes.Actions.PatentActions
+
+import com.AnO.PatentTrademarkManager.intefaces.Action
+import com.fasterxml.jackson.annotation.JsonTypeName
+import java.time.LocalDateTime
+import java.util.*
+import javax.persistence.Entity
+
+@Entity
+@JsonTypeName("change_of_address")
+data class ChangeOfAddressAction(
+        private val type_id:String?=null,
+        override var instruction_ref: UUID?=null,
+
+        val change_of_address_instruction_date: LocalDateTime?=null,
+        val change_of_address_instruction_month: String?=null,
+        val new_address_of_patentee: String?=null,
+        val clerk_of_change_of_address: String?=null,
+        val status_change_of_address: String?=null,
+        val date_received_change_of_address_certificate: LocalDateTime?=null): Action()
+
+/*
+ChangeOfAddressAction {
+change_of_address_instruction_date:
+change_of_address_instruction_month:
+new_address_of_patentee:
+clerk_of_change_of_address:
+status_change_of_address:
+date_received_change_of_address_certificate:
+}
+*/
