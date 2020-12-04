1ef386e3d7e5e50a4d86a22d53529dafffdc43e8
Timi <timi.ogunkeye@gmail.com>
2020-12-04 18:25:36 +0000

    json type rename

--- src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/PatentActions/AssignmentMergerAction.kt
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/PatentActions/AssignmentMergerAction.kt
@@ -3,21 +3,25 @@ package com.AnO.PatentTrademarkManager.classes.Actions.PatentActions
 import com.AnO.PatentTrademarkManager.intefaces.Action
 import com.fasterxml.jackson.annotation.JsonTypeName
 import java.time.LocalDateTime
+import java.util.*
 import javax.persistence.Entity
 
 @Entity
-@JsonTypeName("assignment_merger_action")
+@JsonTypeName("assignment_merger")
 data class AssignmentMergerAction(
-      val assignment_instruction_date:LocalDateTime?=null,
-      val assignment_instruction_month:String?=null,
-      val date_abuja_instructed_assignment:LocalDateTime?=null,
-      val clerk_assignment:String?=null,
-      val status_assignment_registrations:String?=null,
-      val assignor:String?=null,
-      val assignor_address:String?=null,
-      val assignee:String?=null,
-      val assignee_address:String?=null,
-      val date_assignment_certificate_received:LocalDateTime?=null): Action()
+        private val type_id:String?=null,
+        override var instruction_ref: UUID?=null,
+
+        val assignment_instruction_date:LocalDateTime?=null,
+        val assignment_instruction_month:String?=null,
+        val date_abuja_instructed_assignment:LocalDateTime?=null,
+        val clerk_assignment:String?=null,
+        val status_assignment_registrations:String?=null,
+        val assignor:String?=null,
+        val assignor_address:String?=null,
+        val assignee:String?=null,
+        val assignee_address:String?=null,
+        val date_assignment_certificate_received:LocalDateTime?=null): Action()
 
 /*
 AssignmentMergerAction {
