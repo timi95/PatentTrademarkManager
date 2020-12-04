293c1a78541a062661cffd3d441e8776fa2bdf1a
Timi <timi.ogunkeye@gmail.com>
2020-12-04 18:44:51 +0000

    CTCAction.kt added

---
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/PatentActions/CTCAction.kt
@@ -0,0 +1,15 @@
+package com.AnO.PatentTrademarkManager.classes.Actions.PatentActions
+
+class CTCAction {
+}
+
+/*
+CTCAction {
+application_no:
+patent_registration_no:
+ctc_required:
+date_applied_for_ctc:
+ctc_procurement_status:
+clerk_responsible:
+}
+ */
