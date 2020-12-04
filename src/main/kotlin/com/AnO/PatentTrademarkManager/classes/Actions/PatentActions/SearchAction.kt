e680ff68a687b7ade46540560200328f07507cf0
Timi <timi.ogunkeye@gmail.com>
2020-12-04 18:22:47 +0000

    SearchAction.kt data class

--- src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/PatentActions/SearchAction.kt
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/PatentActions/SearchAction.kt
@@ -2,11 +2,27 @@ package com.AnO.PatentTrademarkManager.classes.Actions.PatentActions
 
 import com.AnO.PatentTrademarkManager.intefaces.Action
 import com.fasterxml.jackson.annotation.JsonTypeName
+import java.time.LocalDateTime
 import java.util.*
 import javax.persistence.*
 
 @Entity
-@JsonTypeName("search_action")
+@JsonTypeName("search")
 data class SearchAction(
-        private val type_id:String="search_action",
-        override var instruction_ref: UUID?=null): Action()
+        private val type_id: String?=null,
+        override var instruction_ref: UUID?=null,
+
+        val search_status: String?=null,
+        val reported_to_client: String?=null,
+        val date_reported_to_client: LocalDateTime?=null,
+        val clerk_searching: String?=null): Action()
+
+
+/*
+SearchAction {
+search_status:
+reported_to_client:
+date_reported_to_client:
+clerk_searching:
+}
+*/
