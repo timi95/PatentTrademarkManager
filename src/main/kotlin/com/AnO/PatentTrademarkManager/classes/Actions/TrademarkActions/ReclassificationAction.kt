8f066a78bbf3e74aded75d4f457ae52e0b1ff8ad
Timi <timi.ogunkeye@gmail.com>
2020-12-05 14:28:48 +0000

    jsontype included; fields included

--- src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/TrademarkActions/ReclassificationAction.kt
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/TrademarkActions/ReclassificationAction.kt
@@ -1,7 +1,19 @@
 package com.AnO.PatentTrademarkManager.classes.Actions.TrademarkActions
 
-class ReclassificationAction {
-}
+import com.AnO.PatentTrademarkManager.intefaces.Action
+import com.fasterxml.jackson.annotation.JsonTypeName
+import java.time.LocalDateTime
+import javax.persistence.Entity
+
+@Entity
+@JsonTypeName("reclassification")
+data class ReclassificationAction(
+
+        val date_reclassification_instruction: LocalDateTime?=null,
+        val date_abj_instructed_reclassify: LocalDateTime?=null,
+        val reclassification_status: String?=null,
+        val date_publication_of_reclassification: LocalDateTime?=null,
+        val journal_pg_reclassification: String?=null,): Action()
 
 /*
 T_reclassification_action:any = {
