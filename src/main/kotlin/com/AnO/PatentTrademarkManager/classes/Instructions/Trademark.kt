791d6a169eb79701a32354bafa7da9b4f6925921
Timi <timi.ogunkeye@gmail.com>
2020-12-05 13:49:37 +0000

    some ambiguity on TProfile as action or instruction detail remains

--- src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Instructions/Trademark.kt
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Instructions/Trademark.kt
@@ -11,3 +11,42 @@ data class Trademark(
         override val type_id:String?=null,
         @OneToMany(fetch = FetchType.LAZY, cascade = arrayOf(CascadeType.ALL))
         override val action_list: MutableList<Action>?=null):Instruction()
+
+/*
+T_profile:any = {
+    "applicable_official_fee":
+    "applicable_service_charge":
+    "trademark_class":
+    "clerk_responsible":
+    "clients_email_address":
+    "clients_id":
+    "clients_address":
+    "clients_contact_person":
+    "client_ref_no":
+    "correspondence_date":
+    "current_instruction":
+    "current_status":
+    "date_instruction_received":
+    "date_current_instruction":
+    "date_completed_job_received":
+    "date_incoming_abj_schedule":
+    "date_outgoing_abj_schedule":
+    "filing_receipt":
+    "goods":
+    "incentive_due_clerk":
+    "lawyer_responsible":
+    "month_incoming_schedule":
+    "month_outgoing_schedule":
+    "name_of_client":
+    "name_of_proprietor":
+    "official_fee_ctc":
+    "official_fee_late_renewal_penalty":
+    "official_fee_merger":
+    "official_fee_registered_user":
+    "our_ref_no":
+    "proprietors_address":
+    "quicktellers_fee":
+    "registration_no":
+    "trademark_description":
+};
+ */
