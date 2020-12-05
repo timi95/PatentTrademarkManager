fbd81ac58ea421a0dbb06edf83fec77157893702
Timi <timi.ogunkeye@gmail.com>
2020-12-05 13:47:25 +0000

    RegistrationAction.kt stub added

---
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/TrademarkActions/RegistrationAction.kt
@@ -0,0 +1,36 @@
+package com.AnO.PatentTrademarkManager.classes.Actions.TrademarkActions
+
+class RegistrationAction {
+}
+
+/*
+T_registration_action:any = {
+    acceptance_date:
+    acceptance_facilitation_sent:
+    acknowledgement_date:
+    acknowledgement_facilitation_sent:
+    application_no:
+    trademark_class:
+    clerk_registering:
+    colour_limitation:
+    date_acceptance_facilitation_sent:
+    date_acknowledgement_facilitation_sent:
+    date_of_registration_instruction:
+    date_of_registration_instruction_received:
+    date_registration_cert_facilitation_sent:
+    date_sent_for_publication:
+    date_abj_instructed_for_registration:
+    goods:
+    journal_date:
+    journal_no:
+    journal_page:
+    journal_vol:
+    official_fee_registration:
+    publication:
+    registration_status:
+    registration_filing_month:
+    registration_no:
+    registraion_cert_facilitation_sent:
+    tm_registration_filing_date: ""
+};
+ */
