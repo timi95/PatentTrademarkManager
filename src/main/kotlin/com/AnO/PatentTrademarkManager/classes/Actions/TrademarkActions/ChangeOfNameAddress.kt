39cac8de6e63d51edd4689c11a0c119d17aed271
Timi <timi.ogunkeye@gmail.com>
2020-12-05 13:46:54 +0000

    ChangeOfNameAddress.kt stub added

---
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/TrademarkActions/ChangeOfNameAddress.kt
@@ -0,0 +1,19 @@
+package com.AnO.PatentTrademarkManager.classes.Actions.TrademarkActions
+
+class ChangeOfNameAddress {
+}
+
+/*
+T_change_name_address_action:any = {
+    change_of_address_instruction_date: '',
+    change_of_name_instruction_date: '',
+    date_received_change_of_address_certificate: '',
+    date_received_change_of_name_certificate: '',
+    new_address_of_pptr:
+    new_name_of_pptr:
+    official_fee_change_name:
+    official_fee_change_address:
+    status_change_of_name:
+    status_change_of_address:
+};
+ */
