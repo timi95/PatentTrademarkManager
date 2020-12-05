dee28da184cdcf93c809f47e6c92c9e40f61e811
Timi <timi.ogunkeye@gmail.com>
2020-12-05 14:28:44 +0000

    jsontype included; fields included

--- src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/TrademarkActions/ProcurementOfCertificateAction.kt
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/TrademarkActions/ProcurementOfCertificateAction.kt
@@ -1,7 +1,25 @@
 package com.AnO.PatentTrademarkManager.classes.Actions.TrademarkActions
 
-class ProcurementOfCertificateAction {
-}
+import com.AnO.PatentTrademarkManager.intefaces.Action
+import com.fasterxml.jackson.annotation.JsonTypeName
+import java.time.LocalDateTime
+import javax.persistence.Entity
+
+@Entity
+@JsonTypeName("procurement_of_certificate")
+data class ProcurementOfCertificateAction(
+        val date_procurement_instructed: LocalDateTime?=null,
+        val application_no: String?=null,
+        val trademark_description:String?=null,
+        val journal_no:String?=null,
+        val journal_vol:String?=null,
+        val journal_date:LocalDateTime?=null,
+        val journal_page:String?=null,
+        val date_clerk_instructed_to_procure:LocalDateTime?=null,
+        val cert_procurement_status:String?=null,
+        val date_registration_certificate:LocalDateTime?=null,
+        val registration_no:String?=null,
+        val official_fee_certificate_issuance:String?=null): Action()
 
 /*
 T_certificate_procurement_action:any = {
