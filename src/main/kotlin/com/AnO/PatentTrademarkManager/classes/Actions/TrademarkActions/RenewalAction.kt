ffa495d1bb3fbcf781903eb3cbba575df6866e9c
Timi <timi.ogunkeye@gmail.com>
2020-12-05 13:47:35 +0000

    RenewalAction.kt stub added

---
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/TrademarkActions/RenewalAction.kt
@@ -0,0 +1,21 @@
+package com.AnO.PatentTrademarkManager.classes.Actions.TrademarkActions
+
+class RenewalAction {
+}
+
+/*
+T_renewal_action:any = {
+    date_renewal_cert_facilitation_sent:
+    dt_abj_instructed_renewal:
+    dt_renew_cert_received:
+    next_renewal_due_date:
+    next_renewal_due_month:
+    official_fee_renewal:
+    renewal_cert_facilitation_sent:
+    renewal_due_date:
+    renewal_due_month:
+    renewal_instruction_date:
+    renewal_instruction_month:
+    renewal_status:
+};
+ */
