bc33a497f830ce4f8b4b170064389bfbf0cd9265
Timi <timi.ogunkeye@gmail.com>
2020-12-05 13:47:50 +0000

    SearchAction.kt stub added

---
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/TrademarkActions/SearchAction.kt
@@ -0,0 +1,18 @@
+package com.AnO.PatentTrademarkManager.classes.Actions.TrademarkActions
+
+class SearchAction {
+}
+
+/*
+T_search_action:any = {
+    clerk_searching:
+    conflicting_mark:
+    date_of_search_report:
+    date_reported_to_client:
+    official_search_fee:
+    reported_to_client:
+    search_instruction_date:
+    search_status:
+    search_type:
+};
+ */
