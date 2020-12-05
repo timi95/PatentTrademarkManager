4f07b697adb13d7ff3887bd53566c255306cfe78
Timi <timi.ogunkeye@gmail.com>
2020-12-05 13:47:07 +0000

    ProcurementOfCertificateAction.kt stub added

---
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/TrademarkActions/ProcurementOfCertificateAction.kt
@@ -0,0 +1,21 @@
+package com.AnO.PatentTrademarkManager.classes.Actions.TrademarkActions
+
+class ProcurementOfCertificateAction {
+}
+
+/*
+T_certificate_procurement_action:any = {
+date_procurement_instructed:
+application_no:
+trademark_description:
+journal_no:
+journal_vol:
+journal_date:
+journal_page:
+date_clerk_instructed_to_procure:
+cert_procurement_status:
+date_registration_certificate:
+registration_no:
+official_fee_certificate_issuance:
+};
+ */
