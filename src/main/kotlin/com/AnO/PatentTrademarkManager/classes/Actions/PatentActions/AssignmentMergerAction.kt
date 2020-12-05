214be2b3d19e34070c6ee970b091193135d1b3be
Timi <timi.ogunkeye@gmail.com>
2020-12-05 13:48:31 +0000

    removed trademark details from patent action

--- src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/PatentActions/AssignmentMergerAction.kt
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/PatentActions/AssignmentMergerAction.kt
@@ -41,19 +41,5 @@ assignee_address:
 date_assignment_certificate_received:
 }
 
-T_assignment_merger_action: any = {
-    assignment_instruction_date:
-    assignment_instruction_month:
-    assignee:
-    assignee_address:
-    assignor:
-    assignor_address:
-    clerk_assigning:
-    date_abuja_instructed_assignment:
-    date_assignment_certificate_received:
-    date_facillitation_assignment_cert_sent:
-    date_facillitation_assignment_cert_sent_sent:
-    official_fee_assignment:
-    status_assignment_registrations:
-}
+
  */
