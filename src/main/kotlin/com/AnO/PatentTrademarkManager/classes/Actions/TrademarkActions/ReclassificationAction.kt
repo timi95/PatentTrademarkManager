9934eebb243a9dd8cdfd39a97b93fbc7ac5fa2b2
Timi <timi.ogunkeye@gmail.com>
2020-12-05 13:47:16 +0000

    ReclassificationAction.kt stub added

---
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/TrademarkActions/ReclassificationAction.kt
@@ -0,0 +1,14 @@
+package com.AnO.PatentTrademarkManager.classes.Actions.TrademarkActions
+
+class ReclassificationAction {
+}
+
+/*
+T_reclassification_action:any = {
+    date_reclassification_instruction:
+    date_abj_instructed_reclassify:
+    reclassification_status:
+    date_publication_of_reclassification:
+    journal_pg_reclassification:
+};
+ */
