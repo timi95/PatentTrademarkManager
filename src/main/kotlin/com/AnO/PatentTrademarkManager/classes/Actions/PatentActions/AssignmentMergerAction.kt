5108f115d4573e39507f1dacccc3f9817e2178d6
Timi <timi.ogunkeye@gmail.com>
2020-12-05 13:30:34 +0000

    Adding extra field for Actions that overlap

--- src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/PatentActions/AssignmentMergerAction.kt
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/PatentActions/AssignmentMergerAction.kt
@@ -21,7 +21,11 @@ data class AssignmentMergerAction(
         val assignor_address:String?=null,
         val assignee:String?=null,
         val assignee_address:String?=null,
-        val date_assignment_certificate_received:LocalDateTime?=null): Action()
+        val date_assignment_certificate_received:LocalDateTime?=null,
+        val date_facillitation_assignment_cert_sent:LocalDateTime?=null,
+        val date_facillitation_assignment_cert_sent_sent:LocalDateTime?=null,
+        val official_fee_assignment:String?=null,
+        val clerk_assigning:String?=null): Action()
 
 /*
 AssignmentMergerAction {
@@ -35,5 +39,21 @@ assignor_address:
 assignee:
 assignee_address:
 date_assignment_certificate_received:
+}
+
+T_assignment_merger_action: any = {
+    assignment_instruction_date:
+    assignment_instruction_month:
+    assignee:
+    assignee_address:
+    assignor:
+    assignor_address:
+    clerk_assigning:
+    date_abuja_instructed_assignment:
+    date_assignment_certificate_received:
+    date_facillitation_assignment_cert_sent:
+    date_facillitation_assignment_cert_sent_sent:
+    official_fee_assignment:
+    status_assignment_registrations:
 }
  */
