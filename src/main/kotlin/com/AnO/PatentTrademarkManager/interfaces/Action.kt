b476a52071022ab404078ee2388897770cdf24d4
Timi <timi.ogunkeye@gmail.com>
2020-12-13 17:09:50 +0000

    join column redundant here, still experimenting

--- src/main/kotlin/com/AnO/PatentTrademarkManager/interfaces/Action.kt
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/interfaces/Action.kt
@@ -1,6 +1,7 @@
 package com.AnO.PatentTrademarkManager.intefaces
 
 import com.AnO.PatentTrademarkManager.classes.Actions.PatentActions.P_SearchAction
+import com.AnO.PatentTrademarkManager.classes.Instructions.Patent
 import com.fasterxml.jackson.annotation.JsonSubTypes
 import com.fasterxml.jackson.annotation.JsonTypeInfo
 import java.util.*
@@ -17,7 +18,11 @@ this works for this type info structure
 @JsonTypeInfo(use=JsonTypeInfo.Id.NAME, include=JsonTypeInfo.As.PROPERTY, property="type_id")
 @JsonSubTypes(
         JsonSubTypes.Type(value = P_SearchAction::class, name ="search_action"),)
-abstract class Action(@Id @GeneratedValue open val id:UUID?=null, open var instruction_ref: UUID?=null)
+abstract class Action(@Id @GeneratedValue open val id:UUID?=null,
+                      open var instruction_ref: UUID?=null,
+                      @ManyToOne(cascade = arrayOf(CascadeType.ALL))
+                      @JoinColumn(name = "patent_id")
+                      val patent: Patent?=null)
 
 /* concrete action types
 
