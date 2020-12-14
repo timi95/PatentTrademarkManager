a2becfbd0c388fac02c2b1ead10c81794fb2bcd9
Timi <timi.ogunkeye@gmail.com>
2020-12-14 23:17:25 +0000

    adding action subtypes - incomplete

--- src/main/kotlin/com/AnO/PatentTrademarkManager/interfaces/Action.kt
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/interfaces/Action.kt
@@ -1,6 +1,6 @@
 package com.AnO.PatentTrademarkManager.intefaces
 
-import com.AnO.PatentTrademarkManager.classes.Actions.PatentActions.P_SearchAction
+import com.AnO.PatentTrademarkManager.classes.Actions.PatentActions.*
 import com.AnO.PatentTrademarkManager.classes.Instructions.Patent
 import com.fasterxml.jackson.annotation.JsonSubTypes
 import com.fasterxml.jackson.annotation.JsonTypeInfo
@@ -17,7 +17,11 @@ this works for this type info structure
 @Entity
 @JsonTypeInfo(use=JsonTypeInfo.Id.NAME, include=JsonTypeInfo.As.PROPERTY, property="type_id")
 @JsonSubTypes(
-        JsonSubTypes.Type(value = P_SearchAction::class, name ="search_action"),)
+        JsonSubTypes.Type(value = P_SearchAction::class, name ="P_search_action"),
+        JsonSubTypes.Type(value = P_AssignmentMergerAction::class, name ="P_assignment_merger"),
+        JsonSubTypes.Type(value = P_AmendmentAction::class, name ="P_amendment_action"),
+        JsonSubTypes.Type(value = P_CTCAction::class, name ="P_ctc"),
+        JsonSubTypes.Type(value = P_RenewalAction::class, name ="P_renewal"),)
 abstract class Action(@Id @GeneratedValue open val id:UUID?=null,
                       open var instruction_ref: UUID?=null,
                       @ManyToOne(cascade = arrayOf(CascadeType.ALL))
