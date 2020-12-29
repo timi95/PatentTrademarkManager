51dcaa325b8bd6f19ef8138b470d6f1ae2ca331c
Timi <timi.ogunkeye@gmail.com>
2020-12-29 15:48:49 +0000

    Reminders entity

---
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Reminder.kt
@@ -0,0 +1,16 @@
+package com.AnO.PatentTrademarkManager.classes
+
+import java.time.LocalDateTime
+import java.util.*
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+
+@Entity
+data class Reminder(@GeneratedValue @Id val id: UUID?=null,
+                    val instruction_ref:UUID?=null,
+                    val reminder_creation_time: LocalDateTime = LocalDateTime.now(),
+                    val reminder_date_time: LocalDateTime?=null,
+                    val is_matured: Boolean?=null,
+                    val reminder_message_title:String?=null,
+                    val reminder_message_body:String?=null)
