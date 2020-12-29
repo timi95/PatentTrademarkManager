cdba5c9d0aea7964a0a8048bb96fe74ac555809c
Timi <timi.ogunkeye@gmail.com>
2020-12-29 17:26:54 +0000

    CRUD methods setup

--- src/main/kotlin/com/AnO/PatentTrademarkManager/controllers/ReminderController.kt
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/controllers/ReminderController.kt
@@ -1,11 +1,12 @@
 package com.AnO.PatentTrademarkManager.controllers
 
+import com.AnO.PatentTrademarkManager.classes.Reminder
 import com.AnO.PatentTrademarkManager.services.ReminderService
 import io.swagger.annotations.Api
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.data.domain.Sort
+import org.springframework.web.bind.annotation.*
+import java.util.*
 
 
 @Api(value = "Reminder",
@@ -19,5 +20,27 @@ class ReminderController {
     lateinit var reminderService: ReminderService
 
 //    Reminder CRUD
+    @PostMapping("/reminder")
+    fun createReminder(@RequestBody reminder: Reminder) =
+            reminderService.createReminder(reminder)
+
+    @GetMapping("/reminder/{id}")
+    fun getReminder(@PathVariable id: UUID) =
+            reminderService.getReminder(id)
+
+    @GetMapping("/reminder")
+    fun getReminders(@RequestParam(defaultValue = "0") page:Int,
+                     @RequestParam(defaultValue = "10") size:Int,
+                     @RequestParam direction: Sort.Direction?,
+                     @RequestParam(defaultValue = "id") sort_property:String?) =
+            reminderService.getReminders(page, size, direction=Sort.DEFAULT_DIRECTION, sort_property="id")
+
+    @PutMapping("/reminder/{id}")
+    fun updateReminder(@PathVariable id: UUID, @RequestBody reminder: Reminder) =
+            reminderService.updateReminder(id, reminder)
+
+    @DeleteMapping("/reminder/{id}")
+    fun deleteReminder(@PathVariable id: UUID) =
+            reminderService.deleteReminder(id)
 
 }
