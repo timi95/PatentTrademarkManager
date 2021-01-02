ab9a9a2dc216fe07aa5cd06d447a5eac63b9c1a4
Timi <timi.ogunkeye@gmail.com>
2021-01-02 18:21:24 +0000

    basic pub/sub methods leveraging sseemitter

--- src/main/kotlin/com/AnO/PatentTrademarkManager/services/ReminderService.kt
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/services/ReminderService.kt
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.Sort
 import org.springframework.http.codec.ServerSentEvent
 import org.springframework.stereotype.Service
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
 import reactor.core.publisher.Flux
 import java.time.Duration
 import java.time.LocalDateTime
@@ -20,6 +21,8 @@ import javax.xml.bind.JAXBElement
 
 @Service
 class ReminderService {
+
+    var emitters: MutableList<SseEmitter> = mutableListOf()
     @Autowired
     lateinit var reminderRepository: ReminderRepository
 
@@ -65,6 +68,7 @@ class ReminderService {
     }
 
     fun remindersEvent(): Flux<ServerSentEvent<List<Reminder>>>? {
+
         return Flux.interval(Duration.ofSeconds(5))
                 .map { sequence: Long ->
                     remindersMatured()
@@ -74,5 +78,38 @@ class ReminderService {
                             .data(reminderRepository.findAll())
                             .build()
                 }
+    }
+
+    fun subscribeEvent(): SseEmitter {
+        val emitter = SseEmitter(Long.MAX_VALUE)
+        try {
+            emitter.send{SseEmitter.event().name("INIT")}
+        } catch (e: Exception){
+            throw e
+            emitter.onError{ emitters.remove(emitter) }
+        }
+        emitter.onCompletion{ emitters.remove(emitter) }
+        emitter.onTimeout { emitters.remove(emitter) }
+        emitters.add(emitter)
+        return emitter
+    }
+
+    fun dispatchEvent(reminder: Reminder){
+        emitters.forEach {
+            try {
+              it.send{SseEmitter.event()
+                      .name("reminder-event")
+                      .data(reminder)}
+            } catch (e: Exception){
+                throw e
+                emitters.remove(it)
+            }
+        }
+    }
+
+    fun reminderAnalysis(){
+        while (true){
+
+        }
     }
 }
