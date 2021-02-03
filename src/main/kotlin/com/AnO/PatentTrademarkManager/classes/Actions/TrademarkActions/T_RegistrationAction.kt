8befdd1ad84f351875a3182ac7b8f74b2185aabd
Timi <timi.ogunkeye@gmail.com>
2021-02-03 20:20:14 +0000

    commented fields guide removal

--- src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/TrademarkActions/T_RegistrationAction.kt
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/TrademarkActions/T_RegistrationAction.kt
@@ -29,37 +29,4 @@ data class T_RegistrationAction(
         val registration_status: String?=null,
         val registration_no: String?=null,
         val registration_filing_date: LocalDateTime?=null ,
-        val trademark_description: String?=null,
-): Action()
-
-/*
-T_registration_action:any = {
-    acceptance_date:
-    acceptance_facilitation_sent:
-    acknowledgement_date:
-    acknowledgement_facilitation_sent:
-    application_no:
-    trademark_class:
-    clerk_registering:
-    colour_limitation:
-    date_acceptance_facilitation_sent:
-    date_acknowledgement_facilitation_sent:
-    date_of_registration_instruction:
-    date_of_registration_instruction_received:
-    date_registration_cert_facilitation_sent:
-    date_sent_for_publication:
-    date_abj_instructed_for_registration:
-    goods:
-    journal_date:
-    journal_no:
-    journal_page:
-    journal_vol:
-    official_fee_registration:
-    publication:
-    registration_status:
-    registration_filing_month:
-    registration_no:
-    registraion_cert_facilitation_sent:
-    tm_registration_filing_date: ""
-};
- */
+        val trademark_description: String?=null,): Action()
