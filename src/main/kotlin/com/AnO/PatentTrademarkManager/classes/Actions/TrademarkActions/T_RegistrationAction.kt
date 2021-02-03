9e5e1d8eef1af3a20fb935670b55ab949646b4e6
Timi <timi.ogunkeye@gmail.com>
2021-02-03 17:58:09 +0000

    T_RegistrationAction.kt field changes

--- src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/TrademarkActions/T_RegistrationAction.kt
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/TrademarkActions/T_RegistrationAction.kt
@@ -9,33 +9,40 @@ import javax.persistence.Entity
 @JsonTypeName("registration")
 data class T_RegistrationAction(
 
+        val date_instruction: LocalDateTime?=null,
+        val date_instruction_concluded: LocalDateTime?=null,
+        val date_outgoing_abuja_schedule: LocalDateTime?=null,
+        val date_incoming_abuja_schedule: LocalDateTime?=null,
+
         val acceptance_date: LocalDateTime?=null,
-        val acceptance_facilitation_sent: String?=null,
+//        val acceptance_facilitation_sent: String?=null,
         val acknowledgement_date: LocalDateTime?=null,
-        val acknowledgement_facilitation_sent: String?=null,
+//        val acknowledgement_facilitation_sent: String?=null,
         val application_no: String?=null,
         val trademark_class: String?=null,
-        val clerk_registering: String?=null,
+//        val clerk_registering: String?=null,
         val colour_limitation: String?=null,
-        val date_acceptance_facilitation_sent: LocalDateTime?=null,
-        val date_acknowledgement_facilitation_sent: LocalDateTime?=null,
-        val date_of_registration_instruction: LocalDateTime?=null,
-        val date_of_registration_instruction_received: LocalDateTime?=null,
-        val date_registration_cert_facilitation_sent: LocalDateTime?=null,
+//        val date_acceptance_facilitation_sent: LocalDateTime?=null,
+//        val date_acknowledgement_facilitation_sent: LocalDateTime?=null,
+//        val date_of_registration_instruction: LocalDateTime?=null,
+//        val date_of_registration_instruction_received: LocalDateTime?=null,
+//        val date_registration_cert_facilitation_sent: LocalDateTime?=null,
         val date_sent_for_publication: LocalDateTime?=null,
-        val date_abj_instructed_for_registration: LocalDateTime?=null,
+//        val date_abj_instructed_for_registration: LocalDateTime?=null,
         val goods: String?=null,
         val journal_date: LocalDateTime?=null,
         val journal_no: String?=null,
         val journal_page: String?=null,
         val journal_vol: String?=null,
         val official_fee_registration: String?=null,
-        val publication: String?=null,
+//        val publication: String?=null,
         val registration_status: String?=null,
-        val registration_filing_month: String?=null,
+//        val registration_filing_month: String?=null,
         val registration_no: String?=null,
-        val registraion_cert_facilitation_sent: String?=null,
-        val tm_registration_filing_date: LocalDateTime?=null ): Action()
+//        val registraion_cert_facilitation_sent: String?=null,
+        val registration_filing_date: LocalDateTime?=null ,
+        val trademark_description: String?=null,
+): Action()
 
 /*
 T_registration_action:any = {
