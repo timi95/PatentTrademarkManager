204871d5bc8c30706e116ec7c904b6c65f1e1520
Timi <timi.ogunkeye@gmail.com>
2021-02-03 20:20:08 +0000

    commented fields guide removal

--- src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/TrademarkActions/T_ProcurementOfCertificateAction.kt
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/TrademarkActions/T_ProcurementOfCertificateAction.kt
@@ -24,20 +24,3 @@ data class T_ProcurementOfCertificateAction(
         val date_of_registration_certificate:LocalDateTime?=null,
         val registration_no:String?=null,
         val official_fee_certificate_issuance:String?=null): Action()
-
-/*
-T_certificate_procurement_action:any = {
-date_procurement_instructed:
-application_no:
-trademark_description:
-journal_no:
-journal_vol:
-journal_date:
-journal_page:
-date_clerk_instructed_to_procure:
-cert_procurement_status:
-date_registration_certificate:
-registration_no:
-official_fee_certificate_issuance:
-};
- */
