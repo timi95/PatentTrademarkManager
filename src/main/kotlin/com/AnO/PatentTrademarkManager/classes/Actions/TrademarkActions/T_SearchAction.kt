4cdd9feefbc7724924739df65e253555978a4776
Timi <timi.ogunkeye@gmail.com>
2021-02-03 20:20:20 +0000

    commented fields guide removal

--- src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/TrademarkActions/T_SearchAction.kt
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/TrademarkActions/T_SearchAction.kt
@@ -14,17 +14,3 @@ data class T_SearchAction(
         val official_search_fee: String?=null,
         val search_status: String?=null,
         val search_type: String?=null,): Action()
-
-/*
-T_search_action:any = {
-    clerk_searching:
-    conflicting_mark:
-    date_of_search_report:
-    date_reported_to_client:
-    official_search_fee:
-    reported_to_client:
-    search_instruction_date:
-    search_status:
-    search_type:
-};
- */
