092cdd994f9905dfa17bcdf95946540d9ecc1be3
Timi <timi.ogunkeye@gmail.com>
2021-02-03 16:48:23 +0000

    P_AmendmentAction.kt field changes

--- src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/PatentActions/P_AmendmentAction.kt
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/PatentActions/P_AmendmentAction.kt
@@ -12,12 +12,14 @@ data class P_AmendmentAction(
         private val type_id:String?=null,
         override var instruction_ref: UUID?=null,
 
-        val date_amendment_instruction_received: LocalDateTime?=null,
         val nature_of_amendment:String?=null,
-        val amending_clerk:String?=null,
-        val date_amending_clerk_instructed:LocalDateTime?=null,
+//        val amending_clerk:String?=null,
+//        val date_amending_clerk_instructed:LocalDateTime?=null,
         val status_of_amendment:String?=null,
-        val date_amendment_received: LocalDateTime?=null): Action()
+        val date_instruction: LocalDateTime?=null,
+        val date_instruction_concluded: LocalDateTime?=null,
+        val date_outgoing_abuja_schedule: LocalDateTime?=null,
+        val date_incoming_abuja_schedule: LocalDateTime?=null,): Action()
 /*
 AmendmentAction {
 date_amendment_instruction_received:
