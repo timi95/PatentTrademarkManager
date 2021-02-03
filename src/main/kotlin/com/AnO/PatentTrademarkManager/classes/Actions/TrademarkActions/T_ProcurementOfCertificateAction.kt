99a2b75f73708e8d751554473c2c550d59844866
Timi <timi.ogunkeye@gmail.com>
2021-02-03 17:57:47 +0000

    T_ProcurementOfCertificateAction.kt field changes

--- src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/TrademarkActions/T_ProcurementOfCertificateAction.kt
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/TrademarkActions/T_ProcurementOfCertificateAction.kt
@@ -8,16 +8,22 @@ import javax.persistence.Entity
 @Entity
 @JsonTypeName("procurement_of_certificate")
 data class T_ProcurementOfCertificateAction(
-        val date_procurement_instructed: LocalDateTime?=null,
+
+        val date_instruction: LocalDateTime?=null,
+        val date_instruction_concluded: LocalDateTime?=null,
+        val date_outgoing_abuja_schedule: LocalDateTime?=null,
+        val date_incoming_abuja_schedule: LocalDateTime?=null,
+
+//        val date_procurement_instructed: LocalDateTime?=null,
         val application_no: String?=null,
         val trademark_description:String?=null,
         val journal_no:String?=null,
         val journal_vol:String?=null,
         val journal_date:LocalDateTime?=null,
         val journal_page:String?=null,
-        val date_clerk_instructed_to_procure:LocalDateTime?=null,
+//        val date_clerk_instructed_to_procure:LocalDateTime?=null,
         val cert_procurement_status:String?=null,
-        val date_registration_certificate:LocalDateTime?=null,
+        val date_of_registration_certificate:LocalDateTime?=null,
         val registration_no:String?=null,
         val official_fee_certificate_issuance:String?=null): Action()
 
