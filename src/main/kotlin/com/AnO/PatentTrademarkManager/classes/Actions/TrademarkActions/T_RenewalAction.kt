c78867a7e3a7cbd9f0af19cbd6e2cbbcf3bb8662
Timi <timi.ogunkeye@gmail.com>
2021-02-03 20:20:17 +0000

    commented fields guide removal

--- src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/TrademarkActions/T_RenewalAction.kt
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/TrademarkActions/T_RenewalAction.kt
@@ -17,20 +17,3 @@ data class T_RenewalAction(
         val official_fee_renewal: String?=null,
         val renewal_due_date: LocalDateTime?=null,
         val renewal_status: String?=null): Action()
-
-/*
-T_renewal_action:any = {
-    date_renewal_cert_facilitation_sent:
-    dt_abj_instructed_renewal:
-    dt_renew_cert_received:
-    next_renewal_due_date:
-    next_renewal_due_month:
-    official_fee_renewal:
-    renewal_cert_facilitation_sent:
-    renewal_due_date:
-    renewal_due_month:
-    renewal_instruction_date:
-    renewal_instruction_month:
-    renewal_status:
-};
- */
