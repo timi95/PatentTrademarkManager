f384d6cf4f009f2e729fded918f538ddbd63d765
Timi <timi.ogunkeye@gmail.com>
2021-02-03 16:50:37 +0000

    P_SearchAction.kt field changes

--- src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/PatentActions/P_SearchAction.kt
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/PatentActions/P_SearchAction.kt
@@ -12,10 +12,15 @@ data class P_SearchAction(
         private val type_id: String?=null,
         override var instruction_ref: UUID?=null,
 
+        val date_instruction: LocalDateTime?=null,
+        val date_instruction_concluded: LocalDateTime?=null,
+        val date_outgoing_abuja_schedule: LocalDateTime?=null,
+        val date_incoming_abuja_schedule: LocalDateTime?=null,
         val search_status: String?=null,
         val reported_to_client: String?=null,
-        val date_reported_to_client: LocalDateTime?=null,
-        val clerk_searching: String?=null): Action()
+//        val date_reported_to_client: LocalDateTime?=null,
+//        val clerk_searching: String?=null
+        ): Action()
 
 
 /*
