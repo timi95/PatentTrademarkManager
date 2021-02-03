288578b4ac3bba646648ff08b7facf93881168db
Timi <timi.ogunkeye@gmail.com>
2021-02-03 20:20:05 +0000

    commented fields guide removal

--- src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/TrademarkActions/T_ChangeOfAddressAction.kt
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/TrademarkActions/T_ChangeOfAddressAction.kt
@@ -19,18 +19,3 @@ data class T_ChangeOfAddressAction(
         val official_fee_change_address:String?=null,
         val status_change_of_name:String?=null,
         val status_change_of_address:String?=null): Action()
-
-/*
-T_change_name_address_action:any = {
-    change_of_address_instruction_date: '',
-    change_of_name_instruction_date: '',
-    date_received_change_of_address_certificate: '',
-    date_received_change_of_name_certificate: '',
-    new_address_of_pptr:
-    new_name_of_pptr:
-    official_fee_change_name:
-    official_fee_change_address:
-    status_change_of_name:
-    status_change_of_address:
-};
- */
