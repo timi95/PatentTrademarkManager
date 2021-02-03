b9e2e98f3ff8e95d3bb5444827b020754961c83f
Timi <timi.ogunkeye@gmail.com>
2021-02-03 16:49:33 +0000

    P_CTCAction.kt field changes

--- src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/PatentActions/P_CTCAction.kt
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/PatentActions/P_CTCAction.kt
@@ -12,12 +12,15 @@ data class P_CTCAction(
         private val type_id:String?=null,
         override var instruction_ref: UUID?=null,
 
+        val date_instruction: LocalDateTime?=null,
+        val date_instruction_concluded: LocalDateTime?=null,
+        val date_outgoing_abuja_schedule: LocalDateTime?=null,
+        val date_incoming_abuja_schedule: LocalDateTime?=null,
+
         val application_no: String?=null,
         val ctc_required: String?=null,
-        val date_applied_for_ctc: LocalDateTime?=null,
         val ctc_procurement_status: String?=null,
-        val patent_registration_no: String?=null,
-        val clerk_responsible: String?=null): Action()
+        val patent_registration_no: String?=null, ): Action()
 
 /*
 CTCAction {
