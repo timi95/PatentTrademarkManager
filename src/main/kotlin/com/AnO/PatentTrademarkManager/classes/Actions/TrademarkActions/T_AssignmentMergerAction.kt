6fe93a391be4a4f4047501b1800840ed6608366c
Timi <timi.ogunkeye@gmail.com>
2021-02-03 18:19:07 +0000

    commented fields cleanup

--- src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/TrademarkActions/T_AssignmentMergerAction.kt
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/TrademarkActions/T_AssignmentMergerAction.kt
@@ -17,21 +17,12 @@ data class T_AssignmentMergerAction(
         val date_outgoing_abuja_schedule: LocalDateTime?=null,
         val date_incoming_abuja_schedule: LocalDateTime?=null,
 
-//        val assignment_instruction_date: LocalDateTime?=null,
-//        val assignment_instruction_month:String?=null,
-//        val date_abuja_instructed_assignment: LocalDateTime?=null,
-//        val clerk_assignment:String?=null,
         val status_assignment_registrations:String?=null,
         val assignor:String?=null,
         val assignor_address:String?=null,
         val assignee:String?=null,
         val assignee_address:String?=null,
-//        val date_assignment_certificate_received: LocalDateTime?=null,
-//        val date_facillitation_assignment_cert_sent: LocalDateTime?=null,
-//        val date_facillitation_assignment_cert_sent_sent: LocalDateTime?=null,
-        val official_fee_assignment:String?=null,
-//        val clerk_assigning:String?=null
-        ): Action()
+        val official_fee_assignment:String?=null,): Action()
 
 /*
 T_assignment_merger_action: any = {
