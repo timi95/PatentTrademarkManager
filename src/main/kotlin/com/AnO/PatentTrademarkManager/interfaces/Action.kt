e11e807717a3020b8d42a7abd5ffdc3b7d916dfa
Timi <timi.ogunkeye@gmail.com>
2021-04-07 23:25:25 +0100

    subtype names without hyphens, and -action suffixes

--- src/main/kotlin/com/AnO/PatentTrademarkManager/interfaces/Action.kt
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/interfaces/Action.kt
@@ -17,18 +17,30 @@ this works for this type info structure
 @JsonTypeInfo(use=JsonTypeInfo.Id.NAME, include=JsonTypeInfo.As.PROPERTY, property="type_id")
 @JsonSubTypes(
         JsonSubTypes.Type(value = P_SearchAction::class, name ="search"),
-        JsonSubTypes.Type(value = P_AssignmentMergerAction::class, name ="assignment_merger"),
-        JsonSubTypes.Type(value = P_AmendmentAction::class, name ="amendment_action"),
+        JsonSubTypes.Type(value = P_RenewalAction::class, name ="renewal"),
+        JsonSubTypes.Type(value = P_RegistrationAction::class, name ="registration"),
+        JsonSubTypes.Type(value = P_ProcurementOfCertificateAction::class, name ="procurement"),
         JsonSubTypes.Type(value = P_CTCAction::class, name ="ctc"),
-        JsonSubTypes.Type(value = P_RenewalAction::class, name ="renewal"),)
+        JsonSubTypes.Type(value = P_ChangeOfNameAction::class, name ="change-name"),
+        JsonSubTypes.Type(value = P_ChangeOfAddressAction::class, name ="change-address"),
+        JsonSubTypes.Type(value = P_AssignmentMergerAction::class, name ="assignment-merger"),
+        JsonSubTypes.Type(value = P_AmendmentAction::class, name ="amendment"),)
 abstract class Action(@Id @GeneratedValue open val id:UUID?=null,
                       open var instruction_ref: UUID?=null)
 
 /* concrete action types
 
-Patents
 
 
+    'search',
+    'renewal',
+    'registration',
+    'procurement',
+    'ctc',
+    'change-name',
+    'change-address',
+    'assignment-merger',
+    "amendment",
 
 
 
