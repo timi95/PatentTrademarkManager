f09f19be9e2c53ed668d36ede3ed0d6e4d39f27e
Timi <timi.ogunkeye@gmail.com>
2021-06-15 19:51:43 +0100

    imageEncoded string column added to entity

--- src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Image.kt
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Image.kt
@@ -9,6 +9,7 @@ import javax.persistence.Id
 @Entity
 data class Image(
         @GeneratedValue @Id val id:UUID?=null,
+        val imageEncoded:String?=null,
         val pathString:String?=null,
         val imageName: String?=null,
         val imageSize: Long?=null,
