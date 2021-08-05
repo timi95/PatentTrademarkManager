c08d8a6e67c268c5a12ecbe7fde58f916b7b4e61
Timi <timi.ogunkeye@gmail.com>
2021-08-05 20:29:47 +0100

    T_ReclassificationAction.kt

--- src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/TrademarkActions/T_ReclassificationAction.kt
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/TrademarkActions/T_ReclassificationAction.kt
@@ -3,11 +3,14 @@ package com.AnO.PatentTrademarkManager.classes.Actions.TrademarkActions
 import com.AnO.PatentTrademarkManager.interfaces.Action
 import com.fasterxml.jackson.annotation.JsonTypeName
 import java.time.LocalDateTime
+import java.util.*
 import javax.persistence.Entity
 
 @Entity
 @JsonTypeName("reclassification")
 data class T_ReclassificationAction(
+        private val type_id:String?=null,
+        override var instruction_ref: UUID?=null,
 
         val date_instruction: LocalDateTime?=null,
         val date_instruction_concluded: LocalDateTime?=null,
