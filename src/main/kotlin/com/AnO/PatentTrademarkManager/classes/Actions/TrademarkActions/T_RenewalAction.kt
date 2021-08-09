0a6a26f0da8e7477c7c3ff0cc712216d710cb632
Timi <timi.ogunkeye@gmail.com>
2021-08-09 21:57:46 +0100

    id and ref_id for renewal action

--- src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/TrademarkActions/T_RenewalAction.kt
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/TrademarkActions/T_RenewalAction.kt
@@ -3,11 +3,14 @@ package com.AnO.PatentTrademarkManager.classes.Actions.TrademarkActions
 import com.AnO.PatentTrademarkManager.interfaces.Action
 import com.fasterxml.jackson.annotation.JsonTypeName
 import java.time.LocalDateTime
+import java.util.*
 import javax.persistence.Entity
 
 @Entity
 @JsonTypeName("renewal")
 data class T_RenewalAction(
+        private val type_id:String?=null,
+        override var instruction_ref: UUID?=null,
 
         val date_instruction: LocalDateTime?=null,
         val date_instruction_concluded: LocalDateTime?=null,
