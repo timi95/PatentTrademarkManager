5734d87fad78a46f094c71209680e6002ece158c
Timi <timi.ogunkeye@gmail.com>
2021-08-10 21:52:52 +0100

    SearchAction Id and ref_Id

--- src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/TrademarkActions/T_SearchAction.kt
+++ src/main/kotlin/com/AnO/PatentTrademarkManager/classes/Actions/TrademarkActions/T_SearchAction.kt
@@ -2,8 +2,12 @@ package com.AnO.PatentTrademarkManager.classes.Actions.TrademarkActions
 
 import com.AnO.PatentTrademarkManager.interfaces.Action
 import java.time.LocalDateTime
+import java.util.*
 
 data class T_SearchAction(
+        private val type_id:String?=null,
+        override var instruction_ref: UUID?=null,
+
         val date_instruction: LocalDateTime?=null,
         val date_instruction_concluded: LocalDateTime?=null,
         val date_outgoing_abuja_schedule: LocalDateTime?=null,
